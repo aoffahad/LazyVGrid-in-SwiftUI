836cd3bccd615b820879f61ad40cfbdedd9376fa
aoffahad <aoffahad@gmail.com>
2023-08-16 13:41:14 +0600

    Initial Commit

---
+++ LazyVGrid/LazyVGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVGridApp.swift
+//  LazyVGrid
+//
+//  Created by Fahad on 16/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LazyVGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

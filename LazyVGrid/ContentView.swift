80b6af9322fa61130060ff7557a885d804c6a899
aoffahad <aoffahad@gmail.com>
2023-08-16 16:04:48 +0600

    This is First Commit

--- LazyVGrid/ContentView.swift
+++ LazyVGrid/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columns: [GridItem] = [
+        GridItem(.flexible(),spacing: nil,alignment: nil),
+        GridItem(.flexible(),spacing: nil,alignment: nil),
+        GridItem(.flexible(),spacing: nil,alignment: nil)
+        
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ScrollView {
+            Rectangle()
+                .fill(.red)
+                .frame(height: 400)
+            LazyVGrid(columns: columns,
+                      alignment: .center,
+                      spacing: 5,
+                      pinnedViews: [.sectionHeaders],
+                      content: {
+                
+                
+                Section(
+                    header:
+                        Text("Section 1")
+                        .foregroundColor(.black)
+                        .padding()
+                        .font(.title)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color.blue)){
+                            ForEach(0..<40){ index in
+                                Rectangle()
+                                    .frame(height: 100)
+                                
+                            }
+                        }
+            })
+            
+            LazyVGrid(columns: columns,
+                      alignment: .center,
+                      spacing: 5,
+                      pinnedViews: [.sectionHeaders],
+                      content: {
+                
+                
+                Section(
+                    header:
+                        Text("Section 2")
+                        .foregroundColor(.black)
+                        .padding()
+                        .font(.title)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color.blue))
+                {
+                            ForEach(0..<40){ index in
+                                Rectangle()
+                                    .fill(.green)
+                                    .frame(height: 100)
+                                
+                            }
+                        }
+            })
+            
+            
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
